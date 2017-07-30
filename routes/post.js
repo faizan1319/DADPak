459d448d2d5ea5171495f546bddb6f44eeb6ede7
faizan1319 <faizanullah45@gmail.com>
2017-07-30 15:34:12 +0500

    reomved the error of wrong table name in query

--- routes/post.js
+++ routes/post.js
@@ -11,7 +11,7 @@ router.get('/', function(req, res, next) {
 
 router.get('/trending', function(req, res) {
 	pool.getConnection(function(err, connection) {
-		var sql = "SELECT u.username, u.dp_url, p.post_id, p.post_title, p.post_desc, p.post_lat, p.post_lng, p.post_likes, p.post_dislikes, p.post_comment_count, p.user_id, DATEDIFF(CURRENT_DATE(), posted_date) AS date_diff, TIMEDIFF(CURRENT_TIME(), posted_time) AS time_diff FROM posts p INNER JOIN USER u ON p.user_id = u.user_id ORDER BY post_likes DESC LIMIT 7";
+		var sql = "SELECT u.username, u.dp_url, p.post_id, p.post_title, p.post_desc, p.post_lat, p.post_lng, p.post_likes, p.post_dislikes, p.post_comment_count, p.user_id, DATEDIFF(CURRENT_DATE(), posted_date) AS date_diff, TIMEDIFF(CURRENT_TIME(), posted_time) AS time_diff FROM posts p INNER JOIN user u ON p.user_id = u.user_id ORDER BY post_likes DESC LIMIT 7";
 		connection.query(sql, function(error, results) {
 			res.json(results);
 			connection.release();
