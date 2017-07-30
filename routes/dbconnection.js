c63a1a7912ba879ab75ac860e23c9a367c5a20de
faizan1319 <faizanullah45@gmail.com>
2017-07-30 15:15:57 +0500

    added .env file in .gitignore and changed the db credentials

--- routes/dbconnection.js
+++ routes/dbconnection.js
@@ -1,11 +1,20 @@
 var mysql = require('mysql');
 
+// var pool  = mysql.createPool({
+//   connectionLimit : 100,
+//   host            : 'localhost',
+//   user            : 'root',
+//   password        : '',
+//   database        : 'dod'
+// });
+
+
 var pool  = mysql.createPool({
-  connectionLimit : 100,
-  host            : 'localhost',
-  user            : 'root',
-  password        : '',
-  database        : 'dod'
+  connectionLimit : 10,
+  host            : 'bmsyhziszmhf61g1.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+  user            : 'h5ocpja96rcsw4ty',
+  password        : 'imb4p0bzzf8ek5ho',
+  database        : 'a701xr7ranlad92f'
 });
 
 module.exports = pool;
