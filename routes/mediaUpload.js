982ba8dd726a56b7ddd8b05c2634150c81efe9ea
faizan1319 <faizanullah45@gmail.com>
2017-08-18 07:26:26 -0700

    console the request body in media upload

--- routes/mediaUpload.js
+++ routes/mediaUpload.js
@@ -6,9 +6,11 @@ var pool = require('./dbconnection');
 
 router.post('/postImages', function(req, res) {
 	pool.getConnection(function(err, connection) {
+		console.log(req.body);
 		var sql = "INSERT INTO posts (post_title, post_desc, post_lat, post_lng, user_id) VALUES (?, ?, ?, ?, ?)";
 		var inserts = [req.body.postTitle, req.body.postDesc, req.body.postLat, req.body.postLng, req.body.userId];
 		sql = mysql.format(sql, inserts);
+		console.log(sql);
 		connection.query(sql, function(error, results) {
 			res.json(results);
 			connection.release();
