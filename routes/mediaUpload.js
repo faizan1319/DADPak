e7c7dd0319bb8792f9c8a89f0b1fc88c3bcf1ea1
faizan1319 <faizanullah45@gmail.com>
2017-08-19 09:29:12 -0700

    debugging upload on front end

--- routes/mediaUpload.js
+++ routes/mediaUpload.js
@@ -6,16 +6,16 @@ var pool = require('./dbconnection');
 
 router.post('/postImages', function(req, res) {
 	pool.getConnection(function(err, connection) {
-		console.log(req.files);
-		var sql = "INSERT INTO posts (post_title, post_desc, post_lat, post_lng, user_id) VALUES (?, ?, ?, ?, ?)";
-		var inserts = [req.body.postTitle, req.body.postDesc, req.body.postLat, req.body.postLng, req.body.userId];
-		sql = mysql.format(sql, inserts);
-		console.log(sql);
-		connection.query(sql, function(error, results) {
-			res.json(results);
-			connection.release();
-			if(error) throw error;
-		});
+		res.json(req);
+		// var sql = "INSERT INTO posts (post_title, post_desc, post_lat, post_lng, user_id) VALUES (?, ?, ?, ?, ?)";
+		// var inserts = [req.body.postTitle, req.body.postDesc, req.body.postLat, req.body.postLng, req.body.userId];
+		// sql = mysql.format(sql, inserts);
+		// console.log(sql);
+		// connection.query(sql, function(error, results) {
+		// 	res.json(results);
+		// 	connection.release();
+		// 	if(error) throw error;
+		// });
 	});
 });
 
