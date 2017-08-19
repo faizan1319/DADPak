d8986f812a0e96d7d15cf0a6e452cc2fd456e1af
faizan1319 <faizanullah45@gmail.com>
2017-08-19 01:09:48 -0700

    console log the req files

--- routes/mediaUpload.js
+++ routes/mediaUpload.js
@@ -6,7 +6,7 @@ var pool = require('./dbconnection');
 
 router.post('/postImages', function(req, res) {
 	pool.getConnection(function(err, connection) {
-		console.log(req.body);
+		console.log(req.files);
 		var sql = "INSERT INTO posts (post_title, post_desc, post_lat, post_lng, user_id) VALUES (?, ?, ?, ?, ?)";
 		var inserts = [req.body.postTitle, req.body.postDesc, req.body.postLat, req.body.postLng, req.body.userId];
 		sql = mysql.format(sql, inserts);
