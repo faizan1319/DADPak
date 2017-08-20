084acf8700f00ee6acf36d4258124554bd9e9ae8
faizan1319 <faizanullah45@gmail.com>
2017-08-20 12:13:37 -0700

    debugging

--- routes/mediaUpload.js
+++ routes/mediaUpload.js
@@ -8,12 +8,13 @@ var uploads = multer({dest: storagePath})
 
 var pool = require('./dbconnection');
 
-router.post('/postImages', function(req, res) {
+router.post('/postImages', uploads.single('abc'), function(req, res) {
 	pool.getConnection(function(err, connection) {
 
-		console.log(req.file);
-		console.log(req.body);
-		res.json('kiya khayal ha');
+		console.log('han bhai req.files: ',req.files);
+		console.log('han bhai req.file: ',req.file);
+		console.log('han bhai req.body: ',req.body);
+		res.json('kiya khayal hai');
 		// var sql = "INSERT INTO posts (post_title, post_desc, post_lat, post_lng, user_id) VALUES (?, ?, ?, ?, ?)";
 		// var inserts = [req.body.postTitle, req.body.postDesc, req.body.postLat, req.body.postLng, req.body.userId];
 		// sql = mysql.format(sql, inserts);
