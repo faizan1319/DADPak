8022fd7f1308f50887f110b8e2c62564b6229a41
faizan1319 <faizanullah45@gmail.com>
2017-08-23 11:51:09 -0700

    added upload query

--- routes/mediaUpload.js
+++ routes/mediaUpload.js
@@ -20,32 +20,29 @@ var storagePath = '/app/public/images/postImages/';
 
 
 router.post('/postImages', upload.single('image'), function(req, res) {
+	
+	var userId 				= req.body.userId;
+	var postTitle 			= req.body.postTitle;
+	var postDesc 			= req.body.postDesc;
+	var postLat 			= req.body.postLat;
+	var postLng 			= req.body.postLng;
+	var postMediaType 		= req.body.postMediaType;
+	var postMediaFileName 	= req.file.filename;
+	var postMediaFilePath 	= req.file.destination;
+	var postMediaFileURL 	= req.file.path;
+
+	var inserts = [postTitle, postDesc, postLat, postLng, postMediaType, postMediaFileName, postMediaFilePath, postMediaFileURL, userId];
+	var sql = "INSERT INTO posts (postTitle, postDesc, postLat, postLng, postMediaType, postMediaFileName, postMediaFilePath, postMediaFileURL, user_id) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)";
+	sql = mysql.format(sql, inserts);
+	console.log(sql);
+
 	pool.getConnection(function(err, connection) {
-
-		var body = req.body;
-		var file = req.file;
-
-		var userId 				= req.body.userId;
-		var postTitle 			= req.body.postTitle;
-		var postDesc 			= req.body.postDesc;
-		var postLat 			= req.body.postLat;
-		var postLng 			= req.body.postLng;
-		var postMediaType 		= req.body.postMediaType;
-		var postMediaFileName 	= req.file.filename;
-		var postMediaFilePath 	= req.file.destination;
-		var postMediaFileURL 	= req.file.path;
-
-		var inserts = [postTitle, postDesc, postLat, postLng, postMediaType, postMediaFileName, postMediaFilePath, postMediaFileURL, userId];
-
-		var sql = "INSERT INTO posts (postTitle, postDesc, postLat, postLng, postMediaType, postMediaFileName, postMediaFilePath, postMediaFileURL, user_id) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)";
-		sql = mysql.format(sql, inserts);
-		console.log(sql);
-		res.json(sql);
-		// connection.query(sql, function(error, results) {
-		// 	res.json(results);
-		// 	connection.release();
-		// 	if(error) throw error;
-		// });
+		connection.query(sql, function(error, results) {
+			connection.release();
+			results.message('Successfull!!');
+			res.json(results);
+			if(error) throw error;
+		});
 	});
 });
 
--- routes/videoUpload.js
+++ routes/videoUpload.js
@@ -17,16 +17,29 @@ var storage = multer.diskStorage({
 var upload = multer({ storage: storage})
 
 router.post('/postVideos', upload.single('video'), function(req, res) {
-	pool.getConnection(function(err, connection) {
-		var body = req.body;
-		var file = req.file;
-		console.log(file);
-		// console.log('han bhai req.file: ',req.file);
-		// console.log('han bhai req.body: ',req.body);
-		res.json([body, file]);
-		// connection.query(sql, function(error, results) {
+	
+	var userId 				= req.body.userId;
+	var postTitle 			= req.body.postTitle;
+	var postDesc 			= req.body.postDesc;
+	var postLat 			= req.body.postLat;
+	var postLng 			= req.body.postLng;
+	var postMediaType 		= req.body.postMediaType;
+	var postMediaFileName 	= req.file.filename;
+	var postMediaFilePath 	= req.file.destination;
+	var postMediaFileURL 	= req.file.path;
 
-		// })
+	var inserts = [postTitle, postDesc, postLat, postLng, postMediaType, postMediaFileName, postMediaFilePath, postMediaFileURL, userId];
+	var sql = "INSERT INTO posts (postTitle, postDesc, postLat, postLng, postMediaType, postMediaFileName, postMediaFilePath, postMediaFileURL, user_id) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)";
+	sql = mysql.format(sql, inserts);
+	console.log(sql);
+
+	pool.getConnection(function(err, connection) {
+		connection.query(sql, function(error, results) {
+			connection.release();
+			results.message('Successfull!!');
+			res.json(results);
+			if(error) throw error;
+		})
 	})
 })
 
