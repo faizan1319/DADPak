4c46cad4fcf7d648941addffbfcadbcf3dc13746
faizan1319 <faizanullah45@gmail.com>
2017-09-16 12:43:13 -0700

    removed all bugs of usersubscribed posts

--- routes/post.js
+++ routes/post.js
@@ -11,9 +11,24 @@ router.get('/', function(req, res, next) {
 
 router.get('/trending', function(req, res) {
 	pool.getConnection(function(err, connection) {
-		var sql = "SELECT u.username, u.dpUrl, p.postId, p.postMediaType, p.postMediaFileURL, p.postTitle, p.postDesc, p.postLat, p.postLng, p.postLikes, p.postDislikes, p.postCommentCount, p.userId, DATEDIFF(CURRENT_DATE(), postedDate) AS dateDiff, TIMEDIFF(CURRENT_TIME(), postedTime) AS timeDiff FROM posts p INNER JOIN user u ON p.userId = u.userId ORDER BY postLikes DESC LIMIT 7";
+		var sql = "SELECT u.username, u.dpUrl, p.postId, p.postMediaType, p.postMediaFileURL, p.postTitle, p.postDesc, p.postLat, p.postLng, p.postLikes, p.postDislikes, p.postCommentCount, p.userId, DATEDIFF(CURRENT_DATE(), postedDate) AS dateDiff, TIMEDIFF(CURRENT_TIME(), postedTime) AS timeDiff FROM posts p INNER JOIN user u ON p.userId = u.userId ORDER BY postCommentCount DESC LIMIT 7";
 		connection.query(sql, function(error, results) {
-			res.json(results);
+			var sql2;
+			var inserts2;
+			var loopCount = results.length;
+			for(let x = 0 ; x < loopCount ; x++) {
+				sql2 = "SELECT pca.categoryId, c.categoryName FROM category c INNER JOIN postCategoryAssociation pca ON c.categoryId = pca.categoryId WHERE pca.postId = ?"
+				inserts2 = [results[x].postId];
+				sql2 = mysql.format(sql2, inserts2);
+				connection.query(sql2, function(error2, results2) {
+					if(results2.length != 0 ) {
+						results[x]['postCategories'] = results2;
+					}
+					if(x == (loopCount -1)) {
+						res.json(results);
+					}
+				})
+			}
 			connection.release();
 			if(error) throw error;
 		});
@@ -31,14 +46,12 @@ router.get('/getPostByUserId/:userId', function(req, res) {
 			var inserts2;
 			var loopCount = results.length;
 			for(let x = 0 ; x < loopCount ; x++) {
-				sql2 = "SELECT pca.catId, c.categoryName FROM category c INNER JOIN postCategoryAssociation pca ON c.categoryId = pca.catId WHERE pca.postId = ?"
+				sql2 = "SELECT pca.categoryId, c.categoryName FROM category c INNER JOIN postCategoryAssociation pca ON c.categoryId = pca.categoryId WHERE pca.postId = ?"
 				inserts2 = [results[x].postId];
 				sql2 = mysql.format(sql2, inserts2);
 				connection.query(sql2, function(error2, results2) {
 					if(results2.length != 0 ) {
-						console.log('data: ',results2);
 						results[x]['postCategories'] = results2;
-						console.log('yelo: ',results[x]);
 					}
 					if(x == (loopCount -1)) {
 						res.json(results);
@@ -65,36 +78,34 @@ router.get('/getPostByUserId/:userId', function(req, res) {
 	})
 })
 
-// router.get('/getUserSubscribtionPosts/:userId', function(req, res) {
-// 	var userId = req.params.userId;
-// 	var sql = "SELECT pca.postId, p.postMediaType, p.postMediaFileURL, p.postTitle, p.postDesc, p.postLat, p.postLng, p.postLikes, p.postDislikes, p.postCommentCount, DATEDIFF(CURRENT_DATE(), p.postedDate) AS DATEDIFF, TIMEDIFF(CURRENT_TIME(), p.postedTime) AS TIMEDIFF, us.categoryId FROM posts p INNER JOIN postCategoryAssociation pca ON p.postId = pca.postId INNER JOIN userSubscribtion us ON us.categoryId = pca.categoryId WHERE us.userId = ? GROUP BY pca.postId";
-// 	var inserts = [userId];
-// 	sql = mysql.format(sql, inserts);
-// 	pool.getConnection(function(error, connection) {
-// 		connection.query(sql, function(err, results) {
-// 			var sql2;
-// 			var inserts2;
-// 			var loopCount = results.length;
-// 			for(let x = 0 ; x < loopCount ; x++) {
-// 				sql2 = "SELECT pca.catId, c.categoryName FROM category c INNER JOIN postCategoryAssociation pca ON c.categoryId = pca.catId WHERE pca.postId = ?"
-// 				inserts2 = [results[x].postId];
-// 				sql2 = mysql.format(sql2, inserts2);
-// 				connection.query(sql2, function(error2, results2) {
-// 					if(results2.length != 0 ) {
-// 						console.log('data: ',results2);
-// 						results[x]['postCategories'] = results2;
-// 						console.log('yelo: ',results[x]);
-// 					}
-// 					if(x == (loopCount -1)) {
-// 						res.json(results);
-// 					}
-// 				})
-// 			}
-// 			connection.release();
-// 			if(error) throw error;
-// 		})
-// 	})
-// })
+router.get('/getUserSubscribtionPosts/:userId', function(req, res) {
+	var userId = req.params.userId;
+	var sql = "SELECT u.username, u.dpUrl, pca.postId, p.postMediaType, p.postMediaFileURL, p.postTitle, p.postDesc, p.postLat, p.postLng, p.postLikes, p.postDislikes, p.postCommentCount, DATEDIFF(CURRENT_DATE(), p.postedDate) AS DATEDIFF, TIMEDIFF(CURRENT_TIME(), p.postedTime) AS TIMEDIFF, us.categoryId FROM user u INNER JOIN posts p ON p.userId = u.userId INNER JOIN postCategoryAssociation pca ON p.postId = pca.postId INNER JOIN userSubscribtion us ON us.categoryId = pca.categoryId WHERE us.userId = ? GROUP BY pca.postId";
+	var inserts = [userId];
+	sql = mysql.format(sql, inserts);
+	pool.getConnection(function(error, connection) {
+		connection.query(sql, function(err, results) {
+			var sql2;
+			var inserts2;
+			var loopCount = results.length;
+			for(let x = 0 ; x < loopCount ; x++) {
+				sql2 = "SELECT pca.categoryId, c.categoryName FROM category c INNER JOIN postCategoryAssociation pca ON c.categoryId = pca.categoryId WHERE pca.postId = ?"
+				inserts2 = [results[x].postId];
+				sql2 = mysql.format(sql2, inserts2);
+				connection.query(sql2, function(error2, results2) {
+					if(results2.length != 0 ) {
+						results[x]['postCategories'] = results2;
+					}
+					if(x == (loopCount -1)) {
+						res.json(results);
+					}
+				})
+			}
+			connection.release();
+			if(error) throw error;
+		})
+	})
+})
 
 
 module.exports = router;
