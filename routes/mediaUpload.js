5b5ac5880fefda25b427c30eecee37391ea8f691
faizan1319 <faizanullah45@gmail.com>
2017-10-10 23:19:33 -0700

    adding postlikes to 0 in insert queries query

--- routes/mediaUpload.js
+++ routes/mediaUpload.js
@@ -31,7 +31,7 @@ router.post('/postImages', upload.single('image'), function(req, res) {
 
 
 	var inserts = [postTitle, postDesc, postLat, postLng, postMediaType, postMediaFileName, postMediaFilePath, postMediaFileURL, userId];
-	var sql = "INSERT INTO posts (postTitle, postDesc, postLat, postLng, postMediaType, postMediaFileName, postMediaFilePath, postMediaFileURL, userId, postedTime, postedDate, postCommentCount) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, CURTIME(), CURDATE(), 0)";
+	var sql = "INSERT INTO posts (postTitle, postDesc, postLat, postLng, postMediaType, postMediaFileName, postMediaFilePath, postMediaFileURL, userId, postedTime, postedDate, postCommentCount, postLikes) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, CURTIME(), CURDATE(), 0, 0)";
 	sql = mysql.format(sql, inserts);
 	var sql2 = "INSERT INTO postCategoryAssociation (categoryId, postId, userId) VALUES (?, ?, ?)"
 
