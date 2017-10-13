5801fe576bbe975b03c538154939552e49b05b20
faizan1319 <faizanullah45@gmail.com>
2017-10-13 10:17:17 -0700

    checking cloudinary upload

--- routes/mediaUpload.js
+++ routes/mediaUpload.js
@@ -37,75 +37,75 @@ router.post('/postImages', upload.single('image'), function(req, res) {
 	postMediaFileURL 		= postMediaFileURL.substring(postMediaFileURL.indexOf('images/'), postMediaFileURL.length);
 
 	cloudinary.uploader.upload(req.file.path, function(result) { 
-		console.log('Result of cloudinary upload: ', result); 
-	});
-
-
-	var inserts = [postTitle, postDesc, postLat, postLng, postMediaType, postMediaFileName, postMediaFilePath, postMediaFileURL, userId];
-	var sql = "INSERT INTO posts (postTitle, postDesc, postLat, postLng, postMediaType, postMediaFileName, postMediaFilePath, postMediaFileURL, userId, postedTime, postedDate, postCommentCount, postLikes) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, CURTIME(), CURDATE(), 0, 0)";
-	sql = mysql.format(sql, inserts);
-	var sql2 = "INSERT INTO postCategoryAssociation (categoryId, postId, userId) VALUES (?, ?, ?)"
-
-	pool.getConnection(function(err, connection) {
-		connection.query(sql, function(error, results) {
-			var postId 	= results.insertId;
-			var sql2;
-			var inserts2;
-			var x ;
-			for(x in postCategories) {
-				sql2 	 = "INSERT INTO postCategoryAssociation (categoryId, postId, userId) VALUES (?, ?, ?)"
-				inserts2 = [postCategories[x], postId, userId];
-				console.log(inserts2);
-				sql2 = mysql.format(sql2, inserts2);
-				console.log(sql2);
-				connection.query(sql2, function(error2, results2) {
-					if(error2) throw error2;
-				})
-			}
-			connection.release();
-
-			var sendNotification = function(data) {
-				var headers = {
-					"Content-Type": "application/json; charset=utf-8",
-					"Authorization": "Basic YzEwYjMwZDItNjNiYy00M2EwLWJiZTctOGE5ZjBmMmRlOTk5"
-				};
-				
-				var options = {
-					host: "onesignal.com",
-					port: 443,
-					path: "/api/v1/notifications",
-					method: "POST",
-					headers: headers
-				};
-				
-				var https = require('https');
-				var req = https.request(options, function(res) {  
-					res.on('data', function(data) {
-					console.log("Response:");
-					console.log(JSON.parse(data));
+		postMediaFileURL = result.url;
+		console.log('Result of cloudinary upload: ', result);
+		
+		var inserts = [postTitle, postDesc, postLat, postLng, postMediaType, postMediaFileName, postMediaFilePath, postMediaFileURL, userId];
+		var sql = "INSERT INTO posts (postTitle, postDesc, postLat, postLng, postMediaType, postMediaFileName, postMediaFilePath, postMediaFileURL, userId, postedTime, postedDate, postCommentCount, postLikes) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, CURTIME(), CURDATE(), 0, 0)";
+		sql = mysql.format(sql, inserts);
+		var sql2 = "INSERT INTO postCategoryAssociation (categoryId, postId, userId) VALUES (?, ?, ?)"
+	
+		pool.getConnection(function(err, connection) {
+			connection.query(sql, function(error, results) {
+				var postId 	= results.insertId;
+				var sql2;
+				var inserts2;
+				var x ;
+				for(x in postCategories) {
+					sql2 	 = "INSERT INTO postCategoryAssociation (categoryId, postId, userId) VALUES (?, ?, ?)"
+					inserts2 = [postCategories[x], postId, userId];
+					console.log(inserts2);
+					sql2 = mysql.format(sql2, inserts2);
+					console.log(sql2);
+					connection.query(sql2, function(error2, results2) {
+						if(error2) throw error2;
+					})
+				}
+				connection.release();
+	
+				var sendNotification = function(data) {
+					var headers = {
+						"Content-Type": "application/json; charset=utf-8",
+						"Authorization": "Basic YzEwYjMwZDItNjNiYy00M2EwLWJiZTctOGE5ZjBmMmRlOTk5"
+					};
+					
+					var options = {
+						host: "onesignal.com",
+						port: 443,
+						path: "/api/v1/notifications",
+						method: "POST",
+						headers: headers
+					};
+					
+					var https = require('https');
+					var req = https.request(options, function(res) {  
+						res.on('data', function(data) {
+						console.log("Response:");
+						console.log(JSON.parse(data));
+						});
 					});
-				});
+					
+					req.on('error', function(e) {
+						console.log("ERROR:");
+						console.log(e);
+					});
+					
+					req.write(JSON.stringify(data));
+					req.end();
+				};
+				  
+				var message = { 
+				app_id: "f2c01e69-0fcb-4967-9a65-3e2aa3bd1cce",
+				contents: {"en": "New post from your Subscriptions."},
+				included_segments: ["Active Users"]
+				};
+				  
+				sendNotification(message);
+	
+				res.send(results);
 				
-				req.on('error', function(e) {
-					console.log("ERROR:");
-					console.log(e);
-				});
-				
-				req.write(JSON.stringify(data));
-				req.end();
-			};
-			  
-			var message = { 
-			app_id: "f2c01e69-0fcb-4967-9a65-3e2aa3bd1cce",
-			contents: {"en": "New post from your Subscriptions."},
-			included_segments: ["Active Users"]
-			};
-			  
-			sendNotification(message);
-
-			res.send(results);
-			
-			if(error) throw error;
+				if(error) throw error;
+			});
 		});
 	});
 });
