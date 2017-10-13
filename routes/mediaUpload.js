ea44f4b879e7ca81d930eab9c59ec8428ff9812c
faizan1319 <faizanullah45@gmail.com>
2017-10-13 10:03:28 -0700

    checking cloudinary upload

--- routes/mediaUpload.js
+++ routes/mediaUpload.js
@@ -6,9 +6,9 @@ var pool = require('./dbconnection');
 var cloudinary = require('cloudinary');
 
 cloudinary.config({ 
-	cloud_name: 'sample', 
-	api_key: '874837483274837', 
-	api_secret: 'a676b67565c6767a6767d6767f676fe1' 
+	cloud_name: 'dgwrzv5ah', 
+	api_key: '295718447284411', 
+	api_secret: 'nMHMFC5LMWWhsLQkhfx8INi3tmU' 
   });
 
 var storage = multer.diskStorage({
