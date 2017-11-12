0a3fa2578194d1d27aa4ba5ca2d75e260cbade4a
faizan1319 <faizanullah45@gmail.com>
2017-11-12 19:47:53 +0500

    modified getusersubscription serivce.

--- routes/subscriptions.js
+++ routes/subscriptions.js
@@ -21,7 +21,7 @@ router.get('/', function(req, res) {
 
 router.get('/:userId', function(req, res) {
 	var userId = req.params.userId;
-	var sql = 'SELECT categoryId From userSubscription WHERE userId = ?';
+	var sql = 'SELECT us.categoryId, c.categoryName From userSubscription us INNER JOIN category c ON us.categoryId = c.categoryId WHERE userId = ?';
 	var inserts = [userId];
 	sql = mysql.format(sql, inserts);
 	pool.getConnection(function(error, connection) {
@@ -43,24 +43,31 @@ router.put('/', function(req, res) {
     var del = req.body.del;
     var ins = req.body.ins;
     var userId = req.body.userId;
+    console.log(req.body);
     var sqlForDel = 'DELETE FROM userSubscription WHERE userId = ? AND categoryId = ?';
     var sqlForIns = 'INSERT INTO userSubscription (userId, categoryId) VALUES (?, ?)';
     pool.getConnection(function(error, connection) {
         if(error) res.json(error);
         else {
-            for (let item of del) {
-                inserts = [userId, item];
-                sql = mysql.format(sqlForDel, inserts); 
-                connection.query(sql, function(err, results) {
-                    if(err) res.json(err);
-                })
+            if (del.length > 0) {
+                for (let item of del) {
+                    inserts = [userId, item];
+                    sql = mysql.format(sqlForDel, inserts); 
+                    connection.query(sql, function(err, results) {
+                        console.log('del statment: ',results);
+                        if(err) res.json(err);
+                    })
+                }
             }
-            for (let item of ins) {
-                inserts = [userId, item];
-                sql = mysql.format(sqlForIns, inserts); 
-                connection.query(sql, function(err, results) {
-                    if(err) res.json(err);
-                })
+            if (ins.length > 0) {
+                for (let item of ins) {
+                    inserts = [userId, item];
+                    sql = mysql.format(sqlForIns, inserts); 
+                    connection.query(sql, function(err, results) {
+                        console.log('insert statment: ',results);
+                        if(err) res.json(err);
+                    })
+                }
             }
             connection.release();
             res.json({message: 'Subscriptions are updated!'});
